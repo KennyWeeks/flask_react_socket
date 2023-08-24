2cddbff8ccec1a0469f73c771fc45b57370b5acc
KennyWeeks <weeksgit@gmail.com>
2023-08-24 13:01:25 -0700

    This is the start of a simple application to connect a flack backend with a react frontend.

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_socketio import SocketIO
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!' #This will have to be a hashed secret
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on("connection")
+def idk():
+    print("BITCH")
+    pass
+
+@socketio.on("my event")
+def handle_event(string):
+    print("This is my string " + str(string))
+
+if __name__ == "__main__":
+    print("What")
+    socketio.run(app, port="6500")
+ 
